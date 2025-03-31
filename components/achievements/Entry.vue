5f913973dd4d0d45a3297115805a90093028963d
Xfox20 <136956349+Xfox20@users.noreply.github.com>
2025-03-31 19:33:10 +0200

    Format code

--- components/achievements/Entry.vue
+++ components/achievements/Entry.vue
@@ -8,7 +8,9 @@ const props = defineProps<{
   <div class="mt-4">
     <p class="flex items-center gap-2 justify-between">
       <b class="text-xl font-bold">{{ props.data.name }}</b>
-      <span class="text-gray-400 text-right text-sm">{{ props.data.rank }}</span>
+      <span class="text-gray-400 text-right text-sm">
+        {{ props.data.rank }}
+      </span>
     </p>
     <p class="text-gray-600 text-sm">{{ props.data.date }}</p>
     <p class="mt-1.5 text-gray-700">{{ props.data.description }}</p>
